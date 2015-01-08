070e49282593d903115449982c14d778e2abc3e5
ceshi <ceshi@AiondeiMac.local>
2015-01-08 14:20:00 +0800

    Initial Commit

---
+++ GCDTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDTest
+//
+//  Created by ceshi on 15/1/8.
+//  Copyright (c) 2015年 ceshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCDTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDTest
+//
+//  Created by ceshi on 15/1/8.
+//  Copyright (c) 2015年 ceshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
